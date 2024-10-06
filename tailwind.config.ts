6a844e18402c375a108c21cc67051a0e4f612bed
Michael Carrier <u6038461@utah.edu>
2024-10-06 12:51:35 -0600

    main updates

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      keyframes: {
+        flyInLeft: {
+          '0%': { transform: 'translateX(-100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+      },
+      animation: {
+        'fly-in-left': 'flyInLeft 1s ease-out forwards',
       },
     },
   },
